9e748cb208e709165493b5be43624aa9ff732af1
Axieum <axieum@users.noreply.github.com>
2018-01-11 20:53:19 +1000

    Prepared the mod environment

---
+++ src/main/java/me/axieum/mcmod/droplet/Droplet.java
@@ -0,0 +1,31 @@
+package me.axieum.mcmod.droplet;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Droplet.MOD_ID, name = Droplet.NAME, version = Droplet.VERSION)
+public class Droplet
+{
+    public static final String MOD_ID = "droplet";
+	public static final String NAME = "Droplet";
+    public static final String VERSION = "1.0";
+
+    @Instance(Droplet.MOD_ID)
+	public static Droplet instance;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+    	//
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	//
+    }
+
+}
